4f19f977c466223cfd2aae97ed53a58d30594485
Kumail <k142227@nu.edu.pk>
2023-01-03 22:56:45 +0500

    Added props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name} your age is {props.age}
+      </p>
+    </div>
+  );
+};
+const App = () => {
+  const name = "Kumail";
+  const age = 25;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
 
-export default App
+export default App;
