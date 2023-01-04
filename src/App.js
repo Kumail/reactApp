e865f09c459a0881e3ced6d1496b58f59d7b9dd7
Kumail <k142227@nu.edu.pk>
2023-01-04 22:20:20 +0500

    Increment counter by clicking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  setTimeout(    () => setCounter(counter + 1),    1000  )
-  return (
-    <div>{counter}</div>
-  )
-}
+  const [counter, setCounter] = useState(0);
 
-export default App
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}> plus</button>
+      <button onClick={setToZero}> zero</button>
+    </div>
+  );
+};
+
+export default App;
